a00665d5e9c1edd7c94de30edca11fd07de9fbf9
Mochammad Dicky Hanun Prasetyo <dickyhanunprasetyo@gmail.com>
2024-09-10 11:24:20 +0700

    Upload Jobsheet 1 Pertemuan 1_Pemrograman Mobile

--- Pertemuan 1/aplikasitest1/lib/main.dart
+++ Pertemuan 1/aplikasitest1/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Hello Flutter App'),
+          title: Text('Helloo Flutter App'),
         ),
         body: Center(
           child: Text(
